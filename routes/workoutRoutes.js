c3d2c0473b811736437827245d1e8a4c7f61d5d0
guidoasbun <gdrodrigo8@gmail.com>
2020-03-18 21:56:43 -0700

    routes

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -8,11 +8,28 @@ router.get('/workouts', (req, res) => {
   .catch(e => console.error(e))
 })
 
+// GET workouts in range
+router.get('/workouts/range', (req, res) => {
+  Workout.find().limit(7)
+    .then(workout => res.json(workout))
+    .catch(e => console.error(e))
+  console.log(req.body)
+})
+
 // Post a workout
 router.get('/workouts', (req, res) =>{
   Workout.create({ day: new Date() })
   .then((data) => res.json(data))
   .catch(e => console.error(e))
 })
+
+// PUT one exercise in workout
+router.put('/workouts/:id', (req, res) => {
+  console.log(req.params.id)
+  Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true, runValidators: true })
+    .then(() => res.sendStatus(200))
+    .catch(e => console.error(e))
+})
+
 
 module.exports = router
