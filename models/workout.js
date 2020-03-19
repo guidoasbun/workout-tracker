fa3af94177225d2fca64ab91a351d4cec8ba6db1
guidoasbun <gdrodrigo8@gmail.com>
2020-03-18 22:30:33 -0700

    mod

--- models/workout.js
+++ models/workout.js
@@ -1,30 +1,33 @@
 const { model, Schema } = require('mongoose')
 
-module.exports = model('workout', new Schema({
-  day: {
-    type: Date,
-    required: true
-  },
-  exercises: [
-    {
-      type: {
-        type: String
-      },
-      name: {
-        type: String
-      },
-      duration: {
-        type: Number
-      },
-      reps: {
-        type: Number
-      },
-      sets: {
-        type: Number
-      },
-      distance: {
-        type: Number
+module.exports = model(
+  'workout',
+  new Schema({
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String
+        },
+        name: {
+          type: String
+        },
+        duration: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
       }
-    }
-  ]
-}))
+    ]
+  })
+);
