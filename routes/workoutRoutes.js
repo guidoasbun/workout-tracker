828dce103bea11cf464b8d861c47376aa73aa9dd
guidoasbun <gdrodrigo8@gmail.com>
2020-03-18 22:23:07 -0700

    heroku update

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -1,35 +1,34 @@
-const router = require('express').Router()
-const { Workout } = require('../models')
+const router = require('express').Router();
+const { Workout } = require('../models');
 
 // Get all workouts
 router.get('/workouts', (req, res) => {
   Workout.find()
-  .then(workouts => res.json(workouts))
-  .catch(e => console.error(e))
-})
+    .then(workouts => res.json(workouts))
+    .catch(e => console.error(e));
+});
 
 // GET workouts in range
 router.get('/workouts/range', (req, res) => {
-  Workout.find().limit(7)
+  Workout.find()
+    .limit(7)
     .then(workout => res.json(workout))
-    .catch(e => console.error(e))
-  console.log(req.body)
-})
+    .catch(e => console.error(e));
+  console.log(req.body);
+});
 
 // Post a workout
-router.get('/workouts', (req, res) =>{
-  Workout.create({ day: new Date() })
-  .then((data) => res.json(data))
-  .catch(e => console.error(e))
-})
+router.post('/workouts', (req, res) => {
+  Workout.create(req.body)
+    .then(workout => res.json(workout))
+    .catch(e => console.log(e));
+});
 
 // PUT one exercise in workout
 router.put('/workouts/:id', (req, res) => {
-  console.log(req.params.id)
-  Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true, runValidators: true })
+  Workout.update({ id: req.params.id }, req.body)
     .then(() => res.sendStatus(200))
-    .catch(e => console.error(e))
-})
+    .catch(e => console.log(e));
+});
 
-
-module.exports = router
+module.exports = router;
